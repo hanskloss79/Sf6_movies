9847fd6eba2fa3e0e225fd11456b88f0ad86b6ca
hanskloss79 <piohandz79@gmail.com>
2023-06-06 22:45:08 +0200

    Full CRUD functionality have been created in MovieController. All needed views have been created. MovieFormType.php file has been created for use in views with forms.

--- src/Controller/MovieController.php
+++ src/Controller/MovieController.php
@@ -3,22 +3,151 @@
 namespace App\Controller;
 
 use App\Entity\Movie;
+use App\Form\MovieFormType;
 use App\Repository\MovieRepository;
+use Doctrine\DBAL\Schema\View;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\Request;
 
 class MovieController extends AbstractController
 {
     private $em;
-    public function __construct(EntityManagerInterface $em)
+    private $movieRepository;
+    public function __construct(MovieRepository $movieRepository, EntityManagerInterface $em)
     {
+        $this->movieRepository = $movieRepository;
         $this->em = $em;
-    }
-    
+    }  
     // all movies
+    #[Route('/movies', methods:['GET'], name: 'movies')]
+    public function index(): Response
+    {
+        $movies = $this->movieRepository->findAll();
+        return $this->render('movies/index.html.twig', [
+          'movies' => $movies  
+        ]);
+    }
+    /////////////////////////////////////////////////////////////
+    // create new movie - CREATE operation
+    #[Route('/movies/create', name: 'create_movie', )]
+    public function create(Request $request): Response
+    {       
+        $movie = new Movie();
+        $form = $this->createForm(MovieFormType::class, $movie);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $newMovie = $form->getData();
+
+            $imagePath = $form->get('imagePath')->getData();
+            if ($imagePath) {
+                $newFileName = uniqid() . '.' . $imagePath->guessExtension();
+
+                try {
+                    $imagePath->move(
+                        $this->getParameter('kernel.project_dir') . '/public/uploads',
+                        $newFileName
+                    );
+                } catch (FileException $e) {
+                    return new Response($e->getMessage());
+                }
+
+                // teraz do bazy ododajemy właściwą ścieżkę
+                $newMovie->setImagePath('/uploads/' . $newFileName);
+            }
+
+            $this->em->persist($newMovie);
+            $this->em->flush();
+
+            return $this->redirectToRoute('movies');
+        }
+
+        return $this->render('movies/create.html.twig', [
+            'form' => $form->createView() 
+        ]);
+    }
+    /////////////////////////////////////////////////////////////
+    // edition of movie existing in database - UPDATE operation
+    #[Route('/movies/edit/{id}',name: 'edit_movie')]
+    public function edit($id, Request $request): Response
+    {
+        $movie = $this->movieRepository->find($id);
+        // formularz powiązany z danym rekoredem z tabeli movie
+        $form = $this->createForm(MovieFormType::class, $movie);
+        $form->handleRequest($request);
+        $imagePath = $form->get('imagePath')->getData();
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            if ($imagePath) {
+                // handle image upload
+                if ($movie->getImagePath() !== null) {
+                    if (file_exists($this->getParameter('kernel.project_dir') . '/public' . $movie->getImagePath())) {
+                        $this->getParameter('kernel.project_dir') . '/public' . $movie->getImagePath();
+
+                        $newFileName = uniqid() . '.' . $imagePath->guessExtension();
+                        try {
+                            $imagePath->move(
+                                $this->getParameter('kernel.project_dir') . '/public/uploads',
+                                $newFileName
+                            );
+                        } catch (FileException $e) {
+                            return new Response($e->getMessage());
+                        }
+
+                        $movie->setImagePath('/uploads/' . $newFileName);
+                        $this->em->flush();
+
+                        return $this->redirectToRoute('movies');
+                    }
+                }
+            } else {
+                $movie->setTitle($form->get('title')->getData());
+                $movie->setReleaseYear($form->get('releaseYear')->getData());
+                $movie->setDescription($form->get('description')->getData());
+
+                $this->em->flush();
+                return $this->redirectToRoute('movies');
+            }
+            
+        }
+
+        return $this->render('movies/edit.html.twig', [
+            'movie' => $movie,
+            'form' => $form->createView()
+        ]);
+    }
+    //////////////////////////////////////////////////////////////////////
+    // remove one movie - DELETE operation
+    #[Route('movies/delete/{id}', methods: ['GET', 'DELETE'], name: 'delete_movie')]
+    public function delete($id): Response
+    {
+        $movie = $this->movieRepository->find($id);
+        $this->em->remove($movie);
+        $this->em->flush();
+        
+        return $this->redirectToRoute('movies');
+    }
+
+
+    //////////////////////////////////////////////////////////////////////
+    // one chosen movie - READ operation
+    #[Route('/movies/{id}', methods:['GET'], name: 'show_movie', )]
+    public function show($id): Response
+    {
+        $movie = $this->movieRepository->find($id);
+        
+        return $this->render('movies/show.html.twig', [
+            'movie' => $movie  
+        ]);
+    }
+
+
+    /*
     #[Route('/movies', name: 'movies')]
     public function index(): Response
     {
@@ -40,6 +169,7 @@ class MovieController extends AbstractController
 
         return $this->render('index.html.twig');
     }
+    */
 
     /* inny sposób użycia Repository 
     public function index(MovieRepository $movieRepository): Response
@@ -52,7 +182,7 @@ class MovieController extends AbstractController
     */
     
     
-    
+    /*
     // one movie
     #[Route('/movies/{name}', name: 'movie', methods:['GET', 'HEAD'])]
     public function show($name): JsonResponse
@@ -62,6 +192,7 @@ class MovieController extends AbstractController
             'path' => 'src/Controller/MovieController.php',
         ]);
     }
+    */
     
     // inna metoda definiowania ścieżki używając annotations ale to już przeszłość  
     /**
